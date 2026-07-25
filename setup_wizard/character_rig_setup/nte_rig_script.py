64c1f7b8eaa812e6502f866094093eb9bd9a56b9
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-07-25 18:25:31 -0500

    fix(nte): fingers and other bones

--- setup_wizard/character_rig_setup/nte_rig_script.py
+++ setup_wizard/character_rig_setup/nte_rig_script.py
@@ -89,35 +89,43 @@ def rig_character(
         'Bip001-L-Finger0': 'DEF-thumb.01.L',
         'Bip001-L-Finger01': 'DEF-thumb.02.L',
         'Bip001-L-Finger02': 'DEF-thumb.03.L',
-        'Bip001-L-Finger1': 'DEF-f_index.01.L',
-        'Bip001-L-Finger11': 'DEF-f_index.02.L',
-        'Bip001-L-Finger12': 'DEF-f_index.03.L',
-        'Bip001-L-Finger2': 'DEF-f_middle.01.L',
-        'Bip001-L-Finger21': 'DEF-f_middle.02.L',
-        'Bip001-L-Finger22': 'DEF-f_middle.03.L',
-        'Bip001-L-Finger3': 'DEF-f_ring.01.L',
-        'Bip001-L-Finger31': 'DEF-f_ring.02.L',
-        'Bip001-L-Finger32': 'DEF-f_ring.03.L',
-        'Bip001-L-Finger4': 'DEF-f_pinky.01.L',
-        'Bip001-L-Finger41': 'DEF-f_pinky.02.L',
-        'Bip001-L-Finger42': 'DEF-f_pinky.03.L',
+
+        'Bip001-L-Finger11': 'DEF-f_index.01.L',
+        'Bip001-L-Finger12': 'DEF-f_index.02.L',
+        'Bip001-L-Finger13': 'DEF-f_index.03.L',
+
+        'Bip001-L-Finger21': 'DEF-f_middle.01.L',
+        'Bip001-L-Finger22': 'DEF-f_middle.02.L',
+        'Bip001-L-Finger23': 'DEF-f_middle.03.L',
+
+        'Bip001-L-Finger31': 'DEF-f_ring.01.L',
+        'Bip001-L-Finger32': 'DEF-f_ring.02.L',
+        'Bip001-L-Finger33': 'DEF-f_ring.03.L',
+
+        'Bip001-L-Finger41': 'DEF-f_pinky.01.L',
+        'Bip001-L-Finger42': 'DEF-f_pinky.02.L',
+        'Bip001-L-Finger43': 'DEF-f_pinky.03.L',
 
         # Dedos Mano Derecha
         'Bip001-R-Finger0': 'DEF-thumb.01.R',
         'Bip001-R-Finger01': 'DEF-thumb.02.R',
         'Bip001-R-Finger02': 'DEF-thumb.03.R',
-        'Bip001-R-Finger1': 'DEF-f_index.01.R',
-        'Bip001-R-Finger11': 'DEF-f_index.02.R',
-        'Bip001-R-Finger12': 'DEF-f_index.03.R',
-        'Bip001-R-Finger2': 'DEF-f_middle.01.R',
-        'Bip001-R-Finger21': 'DEF-f_middle.02.R',
-        'Bip001-R-Finger22': 'DEF-f_middle.03.R',
-        'Bip001-R-Finger3': 'DEF-f_ring.01.R',
-        'Bip001-R-Finger31': 'DEF-f_ring.02.R',
-        'Bip001-R-Finger32': 'DEF-f_ring.03.R',
-        'Bip001-R-Finger4': 'DEF-f_pinky.01.R',
-        'Bip001-R-Finger41': 'DEF-f_pinky.02.R',
-        'Bip001-R-Finger42': 'DEF-f_pinky.03.R',
+
+        'Bip001-R-Finger11': 'DEF-f_index.01.R',
+        'Bip001-R-Finger12': 'DEF-f_index.02.R',
+        'Bip001-R-Finger13': 'DEF-f_index.03.R',
+
+        'Bip001-R-Finger21': 'DEF-f_middle.01.R',
+        'Bip001-R-Finger22': 'DEF-f_middle.02.R',
+        'Bip001-R-Finger23': 'DEF-f_middle.03.R',
+
+        'Bip001-R-Finger31': 'DEF-f_ring.01.R',
+        'Bip001-R-Finger32': 'DEF-f_ring.02.R',
+        'Bip001-R-Finger33': 'DEF-f_ring.03.R',
+
+        'Bip001-R-Finger41': 'DEF-f_pinky.01.R',
+        'Bip001-R-Finger42': 'DEF-f_pinky.02.R',
+        'Bip001-R-Finger43': 'DEF-f_pinky.03.R',
 
         # Ojos y Pechos
         'eye_R': 'DEF-eye.R',
@@ -193,27 +201,30 @@ def rig_character(
 
                 parent_name = b_info['parent']
                 target_p = None
+
                 if parent_name:
                     mapped_p = abadidea.get(parent_name, parent_name)
-                    target_p = rig_edit_bones.get(mapped_p) or rig_edit_bones.get(parent_name)
+                    clean_name = mapped_p[4:] if mapped_p.startswith("DEF-") else mapped_p
+                    org_p = "ORG-" + clean_name
+                    target_p = rig_edit_bones.get(org_p) or rig_edit_bones.get(mapped_p) or rig_edit_bones.get(parent_name)
 
                 if not target_p:
                     nl = b_name.lower()
-                    if any(k in nl for k in ["hair", "head", "ear", "horn", "ring"]):
-                        target_p = rig_edit_bones.get("DEF-spine.006") or rig_edit_bones.get("head")
+                    if any(k in nl for k in ["hair", "head", "ear", "horn", "ring", "bone00"]):
+                        target_p = rig_edit_bones.get("ORG-spine.006") or rig_edit_bones.get("DEF-spine.006") or rig_edit_bones.get("head")
                     elif any(k in nl for k in ["qun", "skirt", "tail", "pelvis"]):
-                        target_p = rig_edit_bones.get("DEF-spine") or rig_edit_bones.get("hips")
+                        target_p = rig_edit_bones.get("ORG-spine") or rig_edit_bones.get("DEF-spine") or rig_edit_bones.get("hips")
                     elif any(k in nl for k in ["xiu", "sleeve", "arm", "elbow"]):
                         if ".r" in nl or "_r_" in nl or "_r" in nl:
-                            target_p = rig_edit_bones.get("DEF-forearm.R") or rig_edit_bones.get("DEF-upper_arm.R")
+                            target_p = rig_edit_bones.get("ORG-forearm.R") or rig_edit_bones.get("DEF-forearm.R")
                         else:
-                            target_p = rig_edit_bones.get("DEF-forearm.L") or rig_edit_bones.get("DEF-upper_arm.L")
+                            target_p = rig_edit_bones.get("ORG-forearm.L") or rig_edit_bones.get("DEF-forearm.L")
                     elif any(k in nl for k in ["cloth", "sce"]):
-                        target_p = rig_edit_bones.get("DEF-spine.003") or rig_edit_bones.get("chest")
+                        target_p = rig_edit_bones.get("ORG-spine.003") or rig_edit_bones.get("DEF-spine.003") or rig_edit_bones.get("chest")
                     elif "thigh" in nl:
-                        target_p = rig_edit_bones.get("DEF-thigh.R" if ".r" in nl else "DEF-thigh.L")
+                        target_p = rig_edit_bones.get("ORG-thigh.R" if ".r" in nl else "ORG-thigh.L") or rig_edit_bones.get("DEF-thigh.R" if ".r" in nl else "DEF-thigh.L")
                     elif "calf" in nl:
-                        target_p = rig_edit_bones.get("DEF-shin.R" if ".r" in nl else "DEF-shin.L")
+                        target_p = rig_edit_bones.get("ORG-shin.R" if ".r" in nl else "ORG-shin.L") or rig_edit_bones.get("DEF-shin.R" if ".r" in nl else "DEF-shin.L")
 
                 if target_p:
                     eb.parent = target_p
@@ -234,17 +245,70 @@ def rig_character(
                 if mod.type == 'ARMATURE':
                     mod.object = rigifyr
 
-    print("NTE Rigify generation complete.\n")
+    if use_bone_collections() and rigifyr:
+        hair_coll = rigifyr.data.collections.get("Hair") or rigifyr.data.collections.new("Hair")
+        skirt_coll = rigifyr.data.collections.get("Skirt") or rigifyr.data.collections.new("Skirt")
+        clothes_coll = rigifyr.data.collections.get("Clothes") or rigifyr.data.collections.new("Clothes")
+        face_coll = rigifyr.data.collections.get("Face & Accessories") or rigifyr.data.collections.new("Face & Accessories")
+        deform_coll = rigifyr.data.collections.get("Deform & Helpers") or rigifyr.data.collections.new("Deform & Helpers")
+        others_coll = rigifyr.data.collections.get("Others") or rigifyr.data.collections.new("Others")
 
-    if use_bone_collections():
-        hidden_collections = [
-            "DEF", "ORG", "MCH", "Deformation", "Original", "Mechanism",
-            "Clothes", "Hair", "Tweaks", "Props"
+        for coll in [hair_coll, skirt_coll, clothes_coll, face_coll, deform_coll, others_coll]:
+            coll.is_visible = False
+
+        main_ctrl_keywords = [
+            "root", "torso", "hips", "chest", "neck", "head",
+            "_ik", "_fk", "_tweak", "ctrl", "master", "pitch", "yaw", "roll"
         ]
+
+        for bone in rigifyr.data.bones:
+            b_name = bone.name
+            b_low = b_name.lower()
+
+            # Check if this bone is a main Rigify UI control widget
+            is_main_ctrl = any(kw in b_low for kw in main_ctrl_keywords) and not (
+                b_name.startswith("DEF-") or b_name.startswith("ORG-") or b_name.startswith("MCH-") or
+                "bip" in b_low or "bn_" in b_low or "bone" in b_low or "wq_" in b_low
+            )
+
+            if is_main_ctrl:
+                continue
+
+
+            target_coll = others_coll
+            if any(k in b_low for k in ["hair", "headline", "bone00"]):
+                target_coll = hair_coll
+            elif any(k in b_low for k in ["qun", "skirt", "tail"]):
+                target_coll = skirt_coll
+            elif any(k in b_low for k in ["xiu", "sleeve", "cloth", "sce"]):
+                target_coll = clothes_coll
+            elif any(k in b_low for k in ["horn", "ear", "ring", "eye", "joint", "face"]):
+                target_coll = face_coll
+            elif b_name.startswith("DEF-") or b_name.startswith("ORG-") or b_name.startswith("MCH-"):
+                target_coll = deform_coll
+
+            try:
+                target_coll.assign(bone)
+            except Exception:
+                pass
+            for c in list(bone.collections):
+                if c != target_coll:
+                    try:
+                        c.unassign(bone)
+                    except Exception:
+                        pass
+            bone.hide = True
+
+        hidden_collections = ["DEF", "ORG", "MCH", "Deformation", "Original", "Mechanism", "Tweaks", "Props"]
         for coll_name in hidden_collections:
             coll = rigifyr.data.collections.get(coll_name)
             if coll:
                 coll.is_visible = False
+    elif rigifyr:
+        for bone in rigifyr.data.bones:
+            if bone.name.startswith("DEF-") or bone.name.startswith("ORG-") or bone.name.startswith("MCH-") or "Bn_" in bone.name or "Bone-" in bone.name or "Bip" in bone.name:
+                bone.hide = True
+
 
     for extra_arm in ["metarig"]:
         m_obj = bpy.data.objects.get(extra_arm)
@@ -257,11 +321,12 @@ def rig_character(
     x = original_name.split("_")
     char_name = x[-2] if len(x) >= 2 else original_name
     try:
-        bpy.data.objects["rigify"].users_collection[0].name = char_name
+        if "rigify" in bpy.data.objects:
+            bpy.data.objects["rigify"].users_collection[0].name = char_name
+            bpy.data.objects["rigify"].name = char_name + "Rig"
     except Exception:
         pass
-    if "rigify" in bpy.data.objects:
-        bpy.data.objects["rigify"].name = char_name + "Rig"
+
 
 
 def use_bone_collections():
